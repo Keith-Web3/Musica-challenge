c810233fc7da3b35c46d2469d2a4de9276e384b4
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2022-10-10 17:57:09 +0100

    fixed few bugs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+let nav = document.querySelector('nav')
+
 document.querySelectorAll('.harmburger-toggle').forEach(el => {
   el.addEventListener('click', function () {
-    document.querySelector('nav').classList.toggle('active')
+    nav.classList.toggle('active')
   })
 })
 
@@ -9,3 +11,21 @@ document.querySelectorAll('input[type=range]').forEach(el => {
     el.parentElement.style.setProperty('--progress', `${el.value}%`)
   })
 })
+
+let inter = new IntersectionObserver(
+  entries => {
+    if (!entries[0].isIntersecting) {
+      document.body.style.overflowY = 'unset'
+      document.querySelector('html').style.overflowY = 'unset'
+      return
+    }
+    if (window.innerWidth >= 735) return
+    document.body.style.overflowY = 'hidden'
+    document.querySelector('html').style.overflowY = 'hidden'
+    document.body.scrollIntoView()
+  },
+  {
+    threshold: 0.1,
+  }
+)
+inter.observe(nav)
