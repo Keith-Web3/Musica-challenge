4d6f0f96c777883a73d87b86fde8374365820f2c
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2022-10-13 10:59:33 +0100

    started second page

--- index.js
+++ index.js
@@ -2,18 +2,31 @@ import SPOTIFY_API_KEY from './apikey.js'
 
 const nav = document.querySelector('nav')
 const ranges = document.querySelectorAll('input[type=range]')
+const navItems = document.querySelectorAll('nav li')
 const audio = document.querySelector('audio')
 const playBtn = document.querySelector('#play')
 const container = document.querySelector('.new-releases__container')
 const currMusicImg = document.querySelector('aside > img')
 const currMusicTitle = document.querySelector('.text__title')
 const currMusicArtist = document.querySelector('.text__artist')
+const repeatBtn = document.querySelector('#repeat')
+const shuffleBtn = document.querySelector('#shuffle')
+const nextBtn = document.querySelector('#next')
+const previousBtn = document.querySelector('#previous')
+
 let seekInterval
 container.innerHTML = ''
+let currSong
 
 document.querySelectorAll('.harmburger-toggle').forEach(el => {
   el.addEventListener('click', function () {
     nav.classList.toggle('active')
+  })
+})
+navItems.forEach((item, idx) => {
+  item.addEventListener('click', function () {
+    if (idx === 0) window.location = 'index.html'
+    if (idx === 1) window.location = 'album.html'
   })
 })
 
@@ -89,7 +102,7 @@ const options = {
 
 async function getMusicPlaylist() {
   let res = await fetch(
-    'https://spotify23.p.rapidapi.com/playlist_tracks/?id=37i9dQZF1DX4Wsb4d7NKfP&offset=0&limit=100',
+    'https://spotify23.p.rapidapi.com/playlist_tracks/?id=2cp7ddwhaxSJIKcb9Z8lUd&offset=0&limit=100',
     options
   )
   let data = await res.json()
@@ -105,21 +118,23 @@ async function getMusicPlaylist() {
     } = track
     container.innerHTML += `<div class="new-release" data-audio="${preview_url}">
                               <img src="${url1}" alt="" style="border-radius: 25px; filter: blur(2px);" data-src="${url}" class="ger"/>
-                              <p>${name}</p>
-                              <p class="sub-text">${artist}</p>
+                              <p title='${name}'>${name
+      .slice(0, 15)
+      .padEnd(18, '...')}</p>
+                              <p class="sub-text" title='${artist}'>${artist
+      .slice(0, 25)
+      .padEnd(28, '...')}</p>
                             </div>`
   })
   Array.from(container.children).forEach(child => {
     child.firstElementChild.style.width = '153px'
     lazyLoader.observe(child.firstElementChild)
   })
-  console.log(data)
 }
 getMusicPlaylist()
-document.body.addEventListener('click', function (e) {
-  if (!e.target.closest('.new-release')) return
-  let releaseContainer = e.target.closest('.new-release')
+function chooseSong(releaseContainer) {
   if (audio.src === releaseContainer.dataset.audio) return
+  currSong = releaseContainer
   pause.call(playBtn)
   audio.src = releaseContainer.dataset.audio
   ranges[1].parentElement.style.setProperty('--progress', `0%`)
@@ -132,6 +147,10 @@ document.body.addEventListener('click', function (e) {
   currMusicImg.src = releaseContainer.firstElementChild.src
   currMusicTitle.textContent = releaseContainer.children[1].textContent
   currMusicArtist.textContent = releaseContainer.lastElementChild.textContent
+}
+document.body.addEventListener('click', function (e) {
+  if (!e.target.closest('.new-release')) return
+  chooseSong(e.target.closest('.new-release'))
 })
 audio.addEventListener('play', function () {
   let duration = audio.duration
@@ -140,9 +159,38 @@ audio.addEventListener('play', function () {
     let progress = (audio.currentTime / duration) * 100
     ranges[1].parentElement.style.setProperty('--progress', `${progress}%`)
     ranges[1].value = progress
-    console.log('on')
-  }, 1000)
+  }, 500)
 })
 audio.addEventListener('pause', function () {
   pause.call(playBtn)
+})
+audio.addEventListener('ended', function () {
+  repeatBtn.classList.contains('active') ? audio.play() : null
+  if (shuffleBtn.classList.contains('active')) {
+    let randomChild = [...container.children][
+      Math.floor(Math.random() * container.children.length)
+    ]
+    chooseSong(randomChild)
+    audio.addEventListener('durationchange', () => {
+      play.call(playBtn)
+    })
+  }
+})
+repeatBtn.addEventListener('click', function () {
+  this.classList.toggle('active')
+  shuffleBtn.classList.remove('active')
+})
+shuffleBtn.addEventListener('click', function () {
+  this.classList.toggle('active')
+  repeatBtn.classList.remove('active')
+})
+nextBtn.addEventListener('click', function () {
+  let nextSong = currSong.nextElementSibling
+  if (!nextSong) return
+  chooseSong(nextSong)
+})
+previousBtn.addEventListener('click', function () {
+  let previousSong = currSong.previousElementSibling
+  if (!previousSong) return
+  chooseSong(previousSong)
 })
